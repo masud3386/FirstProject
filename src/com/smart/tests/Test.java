0f5f5864c35b0697ba63b902a6b5fd051a21acc8
masud3386 <masud3386@yahoo.com>
2024-01-06 12:24:23 -0500

    de

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	@org.testng.annotations.Test
 	public void loginTest() {
 		System.out.println("we are learning git and gitgub through the eclipse");
+		System.out.println("we had initial commit");
 		
 	}
 
